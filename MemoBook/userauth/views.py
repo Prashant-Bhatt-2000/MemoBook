b6ae5ff35253b938b4b3f7570f26145d426d56eb
Prashant Bhatt <114800910+Prashant-Bhatt-2000@users.noreply.github.com>
2023-08-05 11:26:28 +0530

    Update views.py

--- MemoBook/userauth/views.py
+++ MemoBook/userauth/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.models import User
 from .models import User, Memo
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
